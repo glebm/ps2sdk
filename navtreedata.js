82e2cd78bea4cbce1780e63d9dc5efb95d59cea3
uyjulian <uyjulian@users.noreply.github.com>
2023-01-14 22:49:46 +0000

    deploy: cef43b8b9ff7cedabc193c93f909dd510f764c99

---
+++ navtreedata.js
@@ -0,0 +1,84 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PS2SDK", "index.html", [
+    [ "PS2SDK - PS2DEV Open Source Project", "index.html", null ],
+    [ "PS2SDK - KERNEL LIBRARY", "md_ee_kernel__r_e_a_d_m_e.html", null ],
+    [ "remote", "md_ee_rpc_remote_remote.html", null ],
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Structure Index", "classes.html", null ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", null ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"__start_8c_source.html",
+"audsrv__rpc_8h.html#adf399af7f59df7577129f158a1295aba",
+"dir_47818b46b1cf08114dcb647bd070ea80.html",
+"dir_eead4ded970bb501941913089bc8e720.html",
+"ee_2kernel_2include_2sifrpc_8h.html#a95300f2d5cfd9951a18bab56beb5e7df",
+"ee__regs_8h.html#ab70459802983d56ffa86fda67f547196",
+"file_xio_8h.html#structfxio__copyfile__packet",
+"globals_defs_v.html",
+"group__packet2__types.html",
+"gs__psm_8h.html",
+"ioctl_8h.html#ac52adb65a4d86a6f83c2cc5fe0dadc9e",
+"iop_2tcpip_2tcpip_2src_2ps2ip_8c_source.html",
+"libcdvd-common_8h.html#a725b887ebe7e4ebb3fb0691a3f55ec97",
+"libgs_8h.html#a48d17e9be52f3eb05bf548b76e7c817d",
+"libgs_8h.html#afee8861e8587aee30972ef126c07328d",
+"libmc_8h.html#a00cb0fe25991fbfb1402c6d7fe347eca",
+"libsecr-common_8h.html#struct_secr_bit_table",
+"osd__config_8h.html#a3ca02bf4eb9ebdd283861e8e035adc13",
+"ps2cam_8h.html#a5fe175c8666c790e3f865dd73c1bf96d",
+"sbusintr_8h.html",
+"smapregs_8h.html#aaa310ae714684e2c7b5d5eff9bbe3ff6",
+"tcpip_8h.html#afaaed3762ce8e9de14fcf73b19964567",
+"xmtapman_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ navtreeindex22.js
@@ -0,0 +1,15 @@
+var NAVTREEINDEX22 =
+{
+"xmtapman_8h_source.html":[5,0,2,7,0,40],
+"xpadman_8h.html":[5,0,2,7,0,41],
+"xpadman_8h.html#ae1355417e57bc1c00b78d35eddfbfc12":[5,0,2,7,0,41,0],
+"xpadman_8h_source.html":[5,0,2,7,0,41],
+"xprintf_8c_source.html":[5,0,2,1,3,0,2],
+"xprintf_8h_source.html":[5,0,2,1,3,0,3],
+"xsio2man_8h.html":[5,0,2,7,0,42],
+"xsio2man_8h_source.html":[5,0,2,7,0,42],
+"xthbase_8h.html":[5,0,2,7,0,43],
+"xthbase_8h_source.html":[5,0,2,7,0,43],
+"xtimrman_8h.html":[5,0,2,7,0,44],
+"xtimrman_8h_source.html":[5,0,2,7,0,44]
+};
